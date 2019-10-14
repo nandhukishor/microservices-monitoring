58ca0b67d040dcbb552917399659bb4851a19d52
dhananjay12 <dhananjay1204@gmail.com>
2019-10-14 02:29:18 +0200

    Sample app with prometheus and grafana

---
+++ sample-app-micrometer-prometheus/src/main/java/com/mynotes/spring/micrometer/prometheus/SimpleController.java
@@ -0,0 +1,15 @@
+package com.mynotes.spring.micrometer.prometheus;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello from test service!!";
+    }
+
+}
